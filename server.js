6ad6b5e8ec5da63e4eddf32fb24d4cf3a18d8805
unknown <mobsters430@gmail.com>
2025-05-21 18:05:52 -0300

    teste2

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
-const WebSocket = require('ws');
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const WebSocket = require('ws');
 
 const PORT = process.env.PORT || 10000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end('Servidor WebSocket está rodando no Render!');
+  let filePath = '.' + req.url;
+  if (filePath === './') filePath = './admin.html'; // página padrão
+
+  const extname = path.extname(filePath);
+  let contentType = 'text/html';
+
+  if (extname === '.js') contentType = 'text/javascript';
+  if (extname === '.css') contentType = 'text/css';
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('Página não encontrada');
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
 });
 
+// WebSocket
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function connection(ws) {
